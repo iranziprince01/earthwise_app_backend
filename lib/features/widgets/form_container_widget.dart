d81966db23aecad9a64f3a5f243bda7ab8945259
Innocent MANZI <116680024+innocentmanzi@users.noreply.github.com>
2024-07-29 03:56:19 +0200

    Update form_container_widget.dart

--- lib/features/widgets/form_container_widget.dart
+++ lib/features/widgets/form_container_widget.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Importing the Flutter material package
 
 class FormContainerWidget extends StatefulWidget {
-  final TextEditingController? controller;
-  final Key? fieldKey;
-  final bool? isPasswordField;
-  final String? hintText;
-  final String? labelText;
-  final String? helperText;
-  final FormFieldSetter<String>? onSaved;
-  final FormFieldValidator<String>? validator;
-  final ValueChanged<String>? onFieldSubmitted;
-  final TextInputType? inputType;
+  final TextEditingController? controller; // Controller for the text field
+  final Key? fieldKey; // Key for the form field
+  final bool? isPasswordField; // Flag to indicate if this is a password field
+  final String? hintText; // Hint text for the text field
+  final String? labelText; // Label text for the text field
+  final String? helperText; // Helper text for the text field
+  final FormFieldSetter<String>? onSaved; // Callback for when the field is saved
+  final FormFieldValidator<String>? validator; // Validator for the field
+  final ValueChanged<String>? onFieldSubmitted; // Callback for when the field is submitted
+  final TextInputType? inputType; // Input type for the text field
 
   const FormContainerWidget(
       {super.key,
@@ -23,50 +23,51 @@ class FormContainerWidget extends StatefulWidget {
       this.onSaved,
       this.validator,
       this.onFieldSubmitted,
-      this.inputType});
+      this.inputType}); // Constructor for FormContainerWidget
 
   @override
-  State<FormContainerWidget> createState() => _FormContainerWidgetState();
+  State<FormContainerWidget> createState() => _FormContainerWidgetState(); // Creating the state for FormContainerWidget
 }
 
 class _FormContainerWidgetState extends State<FormContainerWidget> {
-  bool _obscureText = true;
+  bool _obscureText = true; // Flag to indicate if the text should be obscured
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      width: double.infinity, // Setting the container width to full
       decoration: BoxDecoration(
-        color: Colors.grey.withOpacity(.35),
-        borderRadius: BorderRadius.circular(15),
+        color: Colors.grey.withOpacity(.35), // Setting the background color with opacity
+        borderRadius: BorderRadius.circular(15), // Setting the border radius
       ),
       child: TextFormField(
-        style: const TextStyle(color: Colors.blue),
-        controller: widget.controller,
-        keyboardType: widget.inputType,
-        key: widget.fieldKey,
-        obscureText: widget.isPasswordField == true ? _obscureText : false,
-        onSaved: widget.onSaved,
-        validator: widget.validator,
-        onFieldSubmitted: widget.onFieldSubmitted,
+        style: const TextStyle(color: Colors.blue), // Setting the text style
+        controller: widget.controller, // Assigning the controller
+        keyboardType: widget.inputType, // Assigning the input type
+        key: widget.fieldKey, // Assigning the key
+        obscureText: widget.isPasswordField == true ? _obscureText : false, // Obscuring text if it's a password field
+        onSaved: widget.onSaved, // Assigning the onSaved callback
+        validator: widget.validator, // Assigning the validator
+        onFieldSubmitted: widget.onFieldSubmitted, // Assigning the onFieldSubmitted callback
         decoration: InputDecoration(
-            border: InputBorder.none,
-            filled: true,
-            hintText: widget.hintText,
-            hintStyle: const TextStyle(color: Colors.black45),
+            border: InputBorder.none, // Removing the border
+            filled: true, // Enabling the filled property
+            hintText: widget.hintText, // Setting the hint text
+            hintStyle: const TextStyle(color: Colors.black45), // Setting the hint text style
             suffixIcon: GestureDetector(
                 onTap: () {
                   setState(() {
-                    _obscureText = !_obscureText;
+                    _obscureText = !_obscureText; // Toggling the obscure text flag
                   });
                 },
-                child: widget.isPasswordField == true
+                child: widget.isPasswordField == true // If it's a password field
                     ? Icon(
-                        _obscureText ? Icons.visibility_off : Icons.visibility,
+                        _obscureText ? Icons.visibility_off : Icons.visibility, // Toggling the visibility icon
                         color:
-                            _obscureText == false ? Colors.blue : Colors.grey,
+                            _obscureText == false ? Colors.blue : Colors.grey, // Setting the icon color
                       )
-                    : const Text(""))),
+                    : const Text("")) // If it's not a password field, display an empty text widget
+        ),
       ),
     );
   }
